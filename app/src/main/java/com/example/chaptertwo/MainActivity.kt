23d2266e1fcaad758b723bd909b5376725e11c1f
austi <ajs396s@login.missouristate.edu>
2024-09-12 16:45:49 -0500

    Chapter Two Start 3

--- app/src/main/java/com/example/chaptertwo/MainActivity.kt
+++ app/src/main/java/com/example/chaptertwo/MainActivity.kt
@@ -6,59 +6,96 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import com.example.chaptertwo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var trueButton: Button
     private lateinit var falseButton: Button
+
+    private lateinit var binding:ActivityMainBinding
     //lateinit' allows initializing a not-null property outside of a constructor
+
+    private val questionBank = listOf(
+        Question(R.string.question_australia, true),
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true)
+
+    )
+
+    private var currentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        // setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
+        // trueButton = findViewById(R.id.true_button)
+        // falseButton = findViewById(R.id.false_button)
 
 
-        //https://developer.android.com/develop/ui/views/notifications/snackbar/action
-
-        /*       trueButton.setOnClickListener {
-            val snackBar = Snackbar.make(
-                it,
-                "Correct",
-                Snackbar.LENGTH_LONG
-            )
-            snackBar.show()
-*/
-        trueButton.setOnClickListener {
-            Toast.makeText(
+        binding.trueButton.setOnClickListener {
+           /* Toast.makeText(
                 this,
                 R.string.correct_toast,
                 Toast.LENGTH_SHORT
             )
                 .show()
-        }
-        /*       falseButton.setOnClickListener {
-            val snackBar = Snackbar.make(
-                it,
-                "Incorrect",
-                Snackbar.LENGTH_LONG
-            )
-            snackBar.setTextColor(Color.BLACK)
-            snackBar.setBackgroundTint(Color.RED)
-            snackBar.show()
-        }
- */
 
-        falseButton.setOnClickListener {
-            Toast.makeText(
+            */
+            checkAnswer(true)
+        }
+
+
+        binding.falseButton.setOnClickListener {
+           /* Toast.makeText(
                 this,
                 R.string.incorrect_toast,
                 Toast.LENGTH_SHORT
             )
                 .show()
 
+            */
+            checkAnswer(false)
+
 
         }
+
+        binding.nextButton.setOnClickListener {
+            currentIndex = (currentIndex + 1) % questionBank.size
+           // val questionTextResId = questionBank[currentIndex].textResId
+           // binding.questionTextView.setText(questionTextResId)
+            updateQuestion()
+
+        }
+
+      //  val questionTextResId = questionBank [currentIndex].textResId
+      //  binding.questionTextView.setText(questionTextResId)
+        updateQuestion()
+    }
+    private fun updateQuestion(){
+        val questionTextResId = questionBank [currentIndex].textResId
+        binding.questionTextView.setText(questionTextResId)
+
+    }
+
+    private fun checkAnswer(userAnswer:Boolean){
+        val correctAnswer = questionBank[currentIndex].answer
+
+        val messageResID = if (userAnswer == correctAnswer) {
+            R.string.correct
+        } else{
+            R.string.incorrect
+        }
+
+        Toast.makeText(this,
+            messageResID,
+            Toast.LENGTH_SHORT)
+            .show()
+
+
     }
 }
